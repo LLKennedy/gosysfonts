e9b6999b02150ed9406b965f56df7101a6e50ead
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-05-01 20:09:09 +1000

    add linux shell

---
+++ linuxfonts.go
@@ -0,0 +1,21 @@
+// +build linux
+
+package gosysfonts
+
+import (
+	"github.com/golang/freetype/truetype"
+	"fmt"
+)
+
+// LinuxPool is the linux implementation of Pool
+type LinuxPool struct{}
+
+// New returns a new Pool
+func New() Pool {
+	return LinuxPool{}
+}
+
+// GetFont returns the truetype font corresponding to the font name passed in
+func (pool WinPool) GetFont(name string) (*truetype.Font, error) {
+	return nil, fmt.Errorf("Not implemented")
+}
